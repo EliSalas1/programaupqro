d3d4fb5392617f2e0551894b63cf9a9593888a44
FatimaG10 <fatimagv328@gmail.com>
2024-11-07 00:25:41 -0500

    Se crea la rama mod4
    Se creo el modulo 4, que se encarga de editar el formulario del evento, este tiene 2 botones más (borrar y editar).

--- client-api/src/componentes/modulo4/mod4.js
+++ client-api/src/componentes/modulo4/mod4.js
@@ -1,33 +1,44 @@
 import React from 'react';
-import { Col, Dropdown, Row } from 'react-bootstrap';
+import { Col, Container, Dropdown, Row } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import InputGroup from 'react-bootstrap/InputGroup';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Badge from 'react-bootstrap/Badge';
 
 function Myformularioedit() {
   return (
     <>
-    <div style={{ width: '99%' }}> 
-      {/* cambié esto: width */}
-      <div> 
-         {/* Form Text Example */}
+    <Container> 
+      <Row> 
+        <Col>   
+    
+         {/* Botón de editar para habilitar el formulario  */}
          <Form>
-        <Form.Label htmlFor="inputPassword5" className="text-start w-100">Nombre del evento</Form.Label>
-        <Form.Control
-          type="text"
-          id="inputText"
-          aria-describedby="textHelpBlock"
+
+         <div className="d-flex justify-content-end mt-5"> 
+         <Button variant="outline-success" className="ml-auto" >Editar</Button>{' '}
+         </div>
+
+        {/* Campo para ingresar el nombre del evento */}
+
+        <div className="mt-5"> 
+          <Form.Label htmlFor="inputPassword5" className="text-start w-100">Nombre del evento</Form.Label>
+          <Form.Control
+            type="text"
+            id="inputText"
+            aria-describedby="textHelpBlock"
         />   
         <Form.Text id="textHelpBlock" muted>
          Por favor ingrese su nombre.
-        </Form.Text>     
-      </Form>
+        </Form.Text>  
+        </div>  
 
-     {/* Formulario de texto */}
-     <Form>
+        {/* Campo donde se ingresa la fecha y hora*/}
+
+        <div className="mt-5"> 
         <Form.Group controlId="inputText">
-          <Form.Label htmlFor="inputText" className="text-start w-100">Fecha y Hora del evento</Form.Label>
+          <Form.Label htmlFor="inputText" className="text-start w-100">Fecha y Hora de inicio del evento</Form.Label>
           <Form.Control
             type="datetime-local"
             id="inputDatetime"
@@ -37,28 +48,44 @@ function Myformularioedit() {
             Por favor ingrese su nombre.
           </Form.Text>
         </Form.Group>
-      </Form>
+        </div>
 
-      {/* Dropdown */}
-      <div className="mt-5">
-        <Form.Group>
-          <Dropdown className="w-100">
-            <Dropdown.Toggle variant="success" id="dropdown-basic" className="w-100"  style={{ textTransform: 'none' }}>
-              Tipo de evento
-            </Dropdown.Toggle>
+         {/* Campo donde se ingresa la fecha y hora*/}
 
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Action1</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+         <div className="mt-5"> 
+        <Form.Group controlId="inputText">
+          <Form.Label htmlFor="inputText" className="text-start w-100">Fecha y Hora de finalización del evento</Form.Label>
+          <Form.Control
+            type="datetime-local"
+            id="inputDatetime"
+            aria-describedby="datetimeHelpBlock"
+          />
+          <Form.Text id="datetimeHelpBlock" muted>
+            Por favor ingrese su nombre.
+          </Form.Text>
         </Form.Group>
-      </div>
+        </div>
 
-      {/* Form Text Example */}
-      <Form>
-        <Form.Label htmlFor="inputPassword5" className="text-start w-100">Nombre del ponente</Form.Label>
+
+          {/* Campo que permite escoger el tipo de evento */}
+      <div className="mt-5">
+        <Dropdown as={ButtonGroup} className="w-100" >
+        <Button variant="success"  style={{ textTransform: 'none', textAlign: 'left' }} className="w-100">Tipo de evento</Button>
+
+        <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+
+        <Dropdown.Menu>
+         <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+         <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+         <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+       </Dropdown>
+      </div>  
+
+      {/*Campo para ingresar el nombre del ponente*/ }
+
+      <div className="mt-5"> 
+      <Form.Label htmlFor="inputPassword5" className="text-start w-100">Nombre del ponente</Form.Label>
         <Form.Control
           type="text"
           id="inputText"
@@ -66,26 +93,29 @@ function Myformularioedit() {
         />   
         <Form.Text id="textHelpBlock" muted>
          Por favor ingrese su nombre.
-        </Form.Text>     
-      </Form>
-
-      <div className="mt-5">
-        <Form.Group>
-          <Dropdown className="w-100">
-            <Dropdown.Toggle variant="success" id="dropdown-basic" className="w-100"  style={{ textTransform: 'none' }}>
-              Lugar
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Action1</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Form.Group>
+        </Form.Text>
       </div>
 
-       <div className="mt-5 text-start w-100" > 
+        {/*Campo para seleccionar el lugar del evento */}
+        
+      <div className="mt-5">
+        <Dropdown as={ButtonGroup} className="w-100">
+        <Button variant="success"  style={{ textTransform: 'none', textAlign: 'left' }} className="w-100">Lugar</Button>
+
+        <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+
+        <Dropdown.Menu>
+         <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+         <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+         <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+       </Dropdown>
+      </div>   
+
+      {/*Aquí se pone otro contenedor para las columnas de las carreras */}
+
+      <Container className="mt-5"> 
+     <div className="mt-5 text-start w-100" > 
         <Row> 
            <Col md={6}> 
            {/* Checkbox (Form Check) */}
@@ -146,9 +176,12 @@ function Myformularioedit() {
           </Col>
         </Row>
        </div> 
+    </Container> 
 
-      <Form>
-        <Form.Label htmlFor="inputPassword5" className="text-start w-100">Nombre del organizador</Form.Label>
+    {/*El siguiente campo es de texto para ingresar el nombre del organizador  */}
+
+    <div className="mt-5"> 
+    <Form.Label htmlFor="inputPassword5" className="text-start w-100">Nombre del organizador</Form.Label>
         <Form.Control
           type="text"
           id="inputText"
@@ -156,11 +189,13 @@ function Myformularioedit() {
         />   
         <Form.Text id="textHelpBlock" muted>
          Por favor ingrese su nombre.
-        </Form.Text>     
-      </Form>
+        </Form.Text> 
+    </div>
 
-      <Form>
-        <Form.Label htmlFor="inputPassword5" className="text-start w-100">Cargo del organizador</Form.Label>
+        {/*Campo para ingresar el cargo del organizador del evento */}
+
+      <div className="mt-5"> 
+      <Form.Label htmlFor="inputPassword5" className="text-start w-100">Cargo del organizador</Form.Label>
         <Form.Control
           type="text"
           id="inputText"
@@ -168,29 +203,40 @@ function Myformularioedit() {
         />   
         <Form.Text id="textHelpBlock" muted>
          Por favor ingrese su cargo.
-        </Form.Text>     
-      </Form>
+        </Form.Text> 
+      </div>    
+
+      {/*Campo para que se muestre la cantidad de personas que asistieron al evento */}
+      <div className="mt-5"> 
+      <h3>
+        Cantidad de personas <Badge bg="secondary">New</Badge>
+      </h3>
       </div>
 
-      <InputGroup className="mb-3">
-        <InputGroup.Text>$</InputGroup.Text>
-        <InputGroup.Text>0.00</InputGroup.Text>
-        <Form.Control aria-label="Dollar amount (with dot and two decimal places)" />
-      </InputGroup>
+        {/*Campo para ingresar comentarios del evento */} 
 
-      <FloatingLabel controlId="floatingTextarea2" label="Comments">
+      <div className="mt-5"> 
+      <FloatingLabel controlId="floatingTextarea2" label="Comments" >
         <Form.Control
           as="textarea"
           placeholder="Leave a comment here"
-
         />
       </FloatingLabel>
-
-       <div className="d-flex justify-content-end mt-3"> 
-         <Button variant="outline-success" className="ml-auto" >Guardar</Button>{' '}
       </div>
 
-    </div>
+      {/*Botón de guardar(guarda los cambios que se realizaron en este formulario) */}
+
+
+      <div className="d-flex flex-column flex-md-row justify-content-end mt-5"> 
+        <Button variant="outline-danger" className="me-md-2 mb-2 mb-md-0">Borrar</Button>
+        <Button variant="outline-success">Guardar</Button>
+      </div>
+
+
+      </Form>
+        </Col>
+      </Row>
+    </Container>
     </>
   );
 }
